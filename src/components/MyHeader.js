abb94eb58a91ea9d10f5ebd0db517c3b6e41f52b
pyeong777 <sdp1123@naver.com>
2022-10-12 05:02:43 +0900

    ✨ 헤더 컴포넌트 추가

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,19 +1,24 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+const headerWrap = css({
+    padding: '20px 0',
+    margin: '0 20px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '1px solid #e2e2e2',
+});
+
+const headBtn = css({
+    fontSize: '25px',
+})
 
 const MyHeader = ({ headText, leftChild, rightChild }) => {
     return (
-        <header
-            css={{
-                padding: '20px 0',
-                margin: '0px 20px',
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                borderBottom: '1px solid #e2e2e2',
-            }}>
+        <header css={headerWrap}>
             <div>{leftChild}</div>
-            <div
-                css={{ fontSize: '25px', }}>{headText}</div>
+            <div css={headBtn}>{headText}</div>
             <div>{rightChild}</div>
         </header>
     )
