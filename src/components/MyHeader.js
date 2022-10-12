a776bfaa70e0bdf50d322237e7e42a3b8717663b
pyeong777 <sdp1123@naver.com>
2022-10-12 21:55:10 +0900

    💄 헤더 컴포넌트 flex 부분 변경

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,26 +1,40 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
-const headerWrap = css({
-    padding: '20px 0',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottom: '1px solid #e2e2e2',
-});
-
-const headBtn = css({
-    fontSize: '25px',
-})
-
 const MyHeader = ({ headText, leftChild, rightChild }) => {
     return (
         <header css={headerWrap}>
-            <div>{leftChild}</div>
-            <div css={headBtn}>{headText}</div>
-            <div>{rightChild}</div>
+            <div css={leftWrap}>{leftChild}</div>
+            <div css={midText}>{headText}</div>
+            <div css={rightWrap}>{rightChild}</div>
         </header>
     )
 }
 
 export default MyHeader;
+
+const leftWrap = css({
+    display: 'flex',
+    width: '25%',
+    justifyContent: 'start',
+})
+
+const rightWrap = css({
+    display: 'flex',
+    width: '25%',
+    justifyContent: 'end',
+})
+
+const headerWrap = css({
+    padding: '20px 0',
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '1px solid #e2e2e2',
+});
+
+const midText = css({
+    fontSize: '25px',
+    width: '50%',
+    justifyContent: 'center',
+    textAlign: 'center'
+})
